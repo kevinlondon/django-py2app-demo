48c7ee1f20352e035b8cf2b3298a1ae6b9381f2d
Kevin London <kevinlondon@gmail.com>
2013-03-01 17:53:48 -0800

    Initial commit, working demo site. Will update to be a little more interesting for testing purposes.

---
+++ demosite/settings.py
@@ -0,0 +1,116 @@
+import os
+import yaml
+import sys
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0, PROJECT_ROOT)
+
+
+# get settings from db/settings.yaml (if any missing all will default)
+# this allows customising the database and web server details after deployment
+config = {}
+try:
+  config_file = os.path.join( 'db', 'settings.yaml' )
+  if os.path.exists( config_file ):
+    config = yaml.load( file( config_file, 'r' ))
+except:
+  pass
+
+def get_config( config_yaml, index, default='' ):
+  try:
+    return config_yaml[ index ]
+  except:
+    return default
+
+DEBUG = get_config( config, 'debug', False )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': "db/sentinel_db.sqlite",
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = get_config( config, 'time_zone', 'Australia/Melbourne' )
+LANGUAGE_CODE = get_config( config, 'language_code', 'en-us' )
+
+START_URL = get_config( config, 'start_url', 'http://localhost/admin/' )
+WEB_SERVER_PORT = get_config( config, 'web_server_port', 80 )
+
+ADMINS = (
+# ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'aaaaaasn%1aaaaaaaaaaaaeaagd%1aaaaaa6*(ef()aag$&f$%'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'demosite.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+  'templates',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'fields',
+    'transformations',
+)
+
+
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+MEDIA_URL = '/site_media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, MEDIA_URL.strip("/"))
+
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
---
+++ setup.py
@@ -0,0 +1,145 @@
+from setuptools import setup
+import py2app
+import os
+
+def add_path_tree( base_path, path, skip_dirs=[ '.svn', '.git' ]):
+  path = os.path.join( base_path, path )
+  partial_data_files = []
+  for root, dirs, files in os.walk( os.path.join( path )):
+    sample_list = []
+    for skip_dir in skip_dirs:
+      if skip_dir in dirs:
+        dirs.remove( skip_dir )
+    if files:
+      for filename in files:
+        sample_list.append( os.path.join( root, filename ))
+    if sample_list:
+      partial_data_files.append((
+        root.replace(
+          base_path + os.sep if base_path else '',
+          '',
+          1
+        ),
+        sample_list
+      ))
+  return partial_data_files
+
+
+
+APP = ['demosite.py']
+DATA_FILES = []
+OPTIONS = {'argv_emulation': True, 'packages':["django"]}
+
+py2app_options = {
+    'includes': [
+      ########
+      # demosite imports
+      'demosite.settings',
+      'fields.models',
+      'fields.views',
+      #fields.urls
+      'transformations.models',
+      'transformations.views',
+      #transformations.urls
+      'demosite.audit',
+      'demosite.urls',
+      'manage',
+      'demosite.settings',
+      'celery',
+      ########
+      # mass django import
+      'django.views.generic.list_detail',
+      'django.template.loaders.filesystem',
+      'django.template.loaders.app_directories',
+      'django.middleware.common',
+      'django.contrib.sessions.middleware',
+      'django.contrib.auth.middleware',
+      'django.middleware.common.CommonMiddleware',
+      'django.contrib.sessions.middleware.SessionMiddleware',
+      'django.contrib.auth.middleware.AuthenticationMiddleware',
+      'django.middleware.doc.XViewMiddleware',
+      'django.middleware.doc',
+      'django.contrib.auth',
+      'django.contrib.contenttypes',
+      'django.contrib.sessions',
+      'django.contrib.sessions.backends.db',
+      'django.contrib.sites',
+      'django.contrib.admin',
+      'django.core.cache.backends',
+      'django.db.backends.sqlite3.base',
+      'django.db.backends.sqlite3.introspection',
+      'django.db.backends.sqlite3.creation',
+      'django.db.backends.sqlite3.client',
+      'django.template.defaulttags',
+      'django.template.defaultfilters',
+      'django.template.loader_tags',
+      'django.contrib.admin.urls',
+      'django.conf.urls.defaults',
+      'django.contrib.admin.views.main',
+      'django.core.context_processors',
+      'django.contrib.auth.views',
+      'django.contrib.auth.backends',
+      'django.views.static',
+      'django.contrib.admin.templatetags.adminmedia',
+      'django.contrib.admin.templatetags.adminapplist',
+      'django.contrib.admin.templatetags.admin_list',
+      'django.contrib.admin.templatetags.admin_modify',
+      'django.contrib.admin.templatetags.log',
+      'django.contrib.admin.views.auth',
+      'django.contrib.admin.views.doc',
+      'django.contrib.admin.views.template',
+      'django.conf.urls.shortcut',
+      'django.views.defaults',
+      'django.core.cache.backends.locmem',
+      'django.templatetags.i18n',
+      'django.views.i18n',
+      'django.core.handlers.wsgi',
+      'django.template.loaders.filesystem.load_template_source',
+      'django.template.loaders.app_directories.load_template_source',
+      ########
+      # also used by django?
+      'email.mime.audio',
+      'email.mime.base',
+      'email.mime.image',
+      'email.mime.message',
+      'email.mime.multipart',
+      'email.mime.nonmultipart',
+      'email.mime.text',
+      'email.charset',
+      'email.encoders',
+      'email.errors',
+      'email.feedparser',
+      'email.generator',
+      'email.header',
+      'email.iterators',
+      'email.message',
+      'email.parser',
+      'email.utils',
+      'email.base64mime',
+      'email.quoprimime',
+      'htmlentitydefs',
+      'html5lib',
+      'HTMLParser',
+    ],
+    'packages':["django",]
+}
+
+# Take the first value from the environment variable PYTHON_PATH
+python_path = os.environ[ 'PYTHONPATH' ].split( ';' )[ 0 ]
+
+django_admin_path = os.path.normpath( python_path + '/lib/site-packages/django/contrib/admin' )
+py2app_data_files = []
+
+# django admin files
+py2app_data_files += add_path_tree( django_admin_path, 'templates' )
+py2app_data_files += add_path_tree( django_admin_path, 'media' )
+# project files
+py2app_data_files += add_path_tree( '', 'db' )
+py2app_data_files += add_path_tree( '', 'templates' )
+
+setup(
+    app=APP,
+    data_files=py2app_data_files,
+    options={'py2app': py2app_options},
+    setup_requires=['py2app'],
+)
