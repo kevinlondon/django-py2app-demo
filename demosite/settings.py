5405430e76450ea549ee273cd891b45d2b74bf2b
Kevin London <kevinlondon@gmail.com>
2013-03-01 18:08:12 -0800

    Add basic views, admin support.

--- demosite/settings.py
+++ demosite/settings.py
@@ -72,8 +72,8 @@ SECRET_KEY = 'aaaaaasn%1aaaaaaaaaaaaeaagd%1aaaaaa6*(ef()aag$&f$%'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
